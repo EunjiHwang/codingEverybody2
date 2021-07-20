1c679671a9979e6b29dd16864837b9a468248d14
EunjiHwang <1101j@naver.com>
2021-07-20 16:09:20 +0900

    OOP - 3, 4, 5. 변수와 메소드, 클래스, 인스턴스

---
+++ MyOOP/MyOOP3.java
@@ -0,0 +1,17 @@
+
+public class MyOOP3 {
+	public static void main(String[] args) {
+		Print.delimiter = "----";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
+		
+		Print.delimiter = "****";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
+	}
+	
+}
